29d3e23209223b2cfaa65a9d7090c2e6c1356337
RockPiryt <rockpiryt@gmail.com>
2025-03-19 21:23:09 +0100

    Add CipherParams struct abd func ExecuteCipher

--- Vignere_Cipher/vignere/flagfunc/flagfunc.go
+++ Vignere_Cipher/vignere/flagfunc/flagfunc.go
@@ -11,6 +11,56 @@ import (
 
 //Author: Paulina Kimak
 
+// Struct to store cipher parameters.
+type CipherParams struct {
+	Operation       string
+	InputText       string
+	InputTextHelper string
+	InputKey        string
+	OutputText      string
+	OutputKey       string
+	CipherFunc      func(string, int, int, string) (string, error)
+	KeyFinder       func(string, string) (int, int)
+}
+
+// Generic function to handle Vignere cipher
+func ExecuteCipher(operation string) {
+	params := CipherParams{
+		Operation:  operation,
+	}
+
+	switch operation {
+	case "e":
+		// Encode the text from plain.txt using the key from key.txt and saves the result to crypto.txt
+		params.InputText = "files/plain.txt"
+		params.InputKey = "files/key.txt"
+		params.OutputText = "files/crypto.txt"
+	case "d":
+		// Decode the text from crypto.txt using the key from key.txt and saves the result to decrypt.txt
+		params.InputText = "files/crypto.txt"
+		params.InputKey = "files/key.txt"
+		params.OutputText = "files/decrypt.txt"
+	case "k":
+		// Make cryptanalysis of the text from crypto.txt and saves the result to decrypt.txt
+		params.InputText = "files/crypto.txt"
+		params.OutputText = "files/decrypt.txt"
+		VignereCryptAnalysis(params.InputText, params.OutputText)
+
+	default:
+		fmt.Println("Nieobsługiwana operacja.")
+		return
+	}	
+		CipherOperations(params)
+}
+
+func VignereCryptAnalysis(s1, s2 string) {
+	panic("unimplemented")
+}
+
+func CipherOperations(params CipherParams) {
+	panic("unimplemented")
+}
+
 // Function to create a new file (plain.txt) containing prepared text for encryption.
 func CreatePlainFile() error {
 	// Check if the org.txt file exists.
