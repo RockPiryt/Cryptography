07d8e815e74074a51620f8ffe186a8979f2ce8c7
RockPiryt <rockpiryt@gmail.com>
2025-04-26 13:52:52 +0200

    Add functions to convert img to grayscale and to Read a key from file

--- Blocks_Cipher/block/helpers/helpers.go
+++ Blocks_Cipher/block/helpers/helpers.go
@@ -3,7 +3,7 @@ package helpers
 
 import (
 	"image"
-	"image/bmp"
+	_ "golang.org/x/image/bmp" 
 	"os"
 )
 
@@ -19,4 +19,31 @@ func LoadImage(path string) (image.Image, error) {
 		return nil, err
 	}
 	return img, nil
+}
+
+// convertToGrayscale takes any image and returns its grayscale version.
+// It goes pixel by pixel and copies each color into a new grayscale image.
+func ConvertToGrayscale(img image.Image) *image.Gray {
+	// Create a new grayscale image with the same size as the original
+	grayImg := image.NewGray(img.Bounds())
+
+	// Loop over every pixel 
+	for y := 0; y < img.Bounds().Dy(); y++ {
+		for x := 0; x < img.Bounds().Dx(); x++ {
+			// Get the original pixel color at (x, y)
+			originalColor := img.At(x, y)
+			// The Set() method will automatically convert color to grayscale
+			grayImg.Set(x, y, originalColor)
+		}
+	}
+	return grayImg
+}
+
+func ReadKey(path string) []byte {
+	key, err := os.ReadFile(path)
+	if err != nil {
+		// If the key file is not found, return a default key
+		return []byte("default-key")
+	}
+	return key
 }
--- Blocks_Cipher/block/main.go
+++ Blocks_Cipher/block/main.go
@@ -7,12 +7,17 @@ import (
 )
 
 func main() {
+	// Load the image
 	img, err := helpers.LoadImage("files/plain.bmp")
 	if err != nil {
 		fmt.Println("Error loading image:", err)
 		return
 	}
 
+	// Convert the image to grayscale
 	grayImg := helpers.ConvertToGrayscale(img)
+
+	// Read the key from the file
+	key := helpers.ReadKey("files/key.txt") 
 
 }
